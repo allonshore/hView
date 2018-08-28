4d0f84f67d5e41e9ca793f78176d1d957d6b2832
tree <hongbo@szljfkj.com>
2018-08-28 17:48:12 +0800

    完成button-group
    完成button-group

---
+++ src/button-group.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="g-button-group">
+       <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss" scoped>
+   .g-button-group{
+       display: inline-flex;
+       vertical-align: top;
+       > .g-button{
+           border-radius:0;
+           /*&:not(:first-child){*/
+               /*border-left:none;*/
+           /*}*/
+           margin-left: -1px;
+           &:first-child{
+               border-top-left-radius: var(--border-radius);
+               border-bottom-left-radius: var(--border-radius);
+           }
+           &:last-child{
+               border-top-right-radius: var(--border-radius);
+               border-bottom-right-radius: var(--border-radius);
+           }
+           &:hover{  //鼠标再上面的时候出现完整的边框 hover起来了 0变1
+               position: relative;
+               z-index: 1;
+           }
+       }
+   }
+</style>
