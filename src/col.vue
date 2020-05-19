590dcf56c855913af166a989a98988240861aa92
allonshore <411628468@qq.com>
2020-05-19 22:11:03 +0800

    完善栅格系统
    完善栅格系统

--- src/col.vue
+++ src/col.vue
@@ -1,30 +1,53 @@
 <template>
-    <div class="col">
-      <slot></slot>
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]"
+     :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}">
+        <div style="border: 1px solid green;height:100px">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
-export default {
-    name:"hongboCol",
-    props:{
-      span:Number
+    export default {
+        name: "hongboCol",
+        props: {
+            span: {
+                type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data(){
+            return{
+                gutter: 0
+            }
+        },
+        created() {
+        },
+        mounted() {
+        }
     }
-}
 </script>
 <style lang="scss" scoped>
-.col{
-  height: 100px;
-  background: grey;
-  width: 50%;
-  border: 1px solid red;
-
-  $class: col- ;
-  //循环1到24
-  @for $n from 1 through 24{
-      // for each $col_#{i}
-      &.#{$class}#{$n}{
-        width: ($n /24) *100%;
-      }
-  }
-}
+    .col {
+        height: 100px;
+        /*background: grey;*/
+        width: 50%;
+        /*border: 1px solid red;*/
+        padding: 0 10px;
+        $class: col- ;
+        //循环1到24
+        @for $n from 1 through 24 {
+            // for each $col_#{i}
+            &.#{$class}#{$n} {
+                width: ($n /24) *100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n /24) *100%;
+            }
+        }
+    }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,15 +1,36 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 <script>
 export default {
-    
+    name:"HongboRow",
+    props:{
+        gutter:{
+            type:[Number,String]
+        }
+    },
+    created() {
+        console.log(this.$children)//没有儿子
+
+    },
+    mounted() {
+       console.log(this.$children)
+        //将gutter传到每个子组件了
+        this.$children.forEach((vm)=>{
+            vm.gutter = this.gutter
+        })
+    }
 }
+ // var div = document.createElement('div')//created 内存中创建出来
+ // var childDiv = document.createElement('div')//child created 内存中创建出来
+ // div.appendChild(childDiv) //child mounted
+ // document.body.appendChild(div)//mounted
 </script>
 <style lang="scss" scoped>
 .row{
     display: flex;
+    /* row补足col左右边框   */
 }
 </style>
